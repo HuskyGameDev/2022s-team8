358d35b741c71ccbbc109111aa5b8e0f19ed7fea
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2022-03-21 19:38:28 -0400

    Basic controllers
    Added basic character and camera controls that allow the player to move and face the users mouse, while the camera keeps the player on screen. Ignore the monkey, I get lazy with sprites sometimes.

---
+++ Restart Robot map/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public Vector2 mousePos;
+    public float moveSpeed = 5f;
+    public Camera cam;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
+        rb.rotation = angle;
+    }
+}
