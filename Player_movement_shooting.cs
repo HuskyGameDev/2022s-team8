3bc3db70553422dd258f572ffee7cdba29cd7521
Johnathan Oestringer <77365910+CrossRyu@users.noreply.github.com>
2022-04-14 21:42:39 -0400

    Update
    updated the scripst

--- Player_movement_shooting.cs
+++ Player_movement_shooting.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousPosition);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
